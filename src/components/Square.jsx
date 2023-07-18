8ee0a9415cd22bfe8eca203c5acce2482e04b2d5
Mauro Romo <mauroromo.developer@outlook.com>
2023-07-18 12:28:00 -0300

    terminando el tic tac toe

---
+++ src/components/Square.jsx
@@ -0,0 +1,13 @@
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const clasName = `square ${isSelected ? "is-selected" : ""}`;
+  const handleClick = () => {
+    updateBoard(index);
+  };
+  return (
+    <div onClick={handleClick} className={clasName}>
+      {children}
+    </div>
+  );
+};
+
+export default Square;
